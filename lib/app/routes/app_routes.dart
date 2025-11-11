7622a9fc7c51e1837bb59bca543a3becffe5068e
Jagadish <adhikari.jagdish5200@gmail.com>
2025-11-11 09:12:54 +0545

    Added Getx and Go Router

---
+++ lib/app/routes/app_routes.dart
@@ -0,0 +1,22 @@
+import 'package:accu_trips/modules/splash/splash_view.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+final GoRouter router = GoRouter(
+  initialLocation: '/',
+  debugLogDiagnostics: kDebugMode,
+  routes: [
+    GoRoute(
+      path: '/',
+      name: 'splash',
+      builder: (context, state) {
+        return const SplashView();
+      },
+    ),
+  ],
+  errorBuilder: (context, state) => Scaffold(
+    appBar: AppBar(title: const Text('Error')),
+    body: Center(child: Text('Page not found: ${state.uri}')),
+  ),
+);
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:accu_trips/app/routes/app_routes.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
+  // Global GetX config (recommended for production)
+  Get.config(enableLog: kDebugMode);
   runApp(const MyApp());
 }
 
@@ -10,12 +15,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-      ),
-      home: Text('hello'),
+    return GetMaterialApp(
+      smartManagement: SmartManagement.onlyBuilder, // auto-dispose unused controllers
+      title: 'AccuTrips',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: true, colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue)),
+
+      home: MaterialApp.router(routerConfig: router, debugShowCheckedModeBanner: false),
     );
   }
 }
